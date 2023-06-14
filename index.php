d973f1dddc9168223c8918393029c6a35a5fcaaf
willymocha <willy.mochamad@neuronworks.co.id>
2023-06-14 17:26:59 +0700

    add more lines

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ class User
     private $password;
     private $role;
 
-    public function __construct($username, $email, $password, $role)
+    public function construct($username, $email, $password, $role)
     {
         $this->username = $username;
         $this->email = $email;
@@ -82,7 +82,7 @@ class Authentication
 {
     private $userDatabase;
 
-    public function __construct($userDatabase)
+    public function construct($userDatabase)
     {
         $this->userDatabase = $userDatabase;
     }
@@ -138,7 +138,7 @@ class Article
     private $ratings;
     private $reviews;
 
-    public function __construct($id, $data)
+    public function construct($id, $data)
     {
         $this->id = $id;
         $this->title = $data['title'];
@@ -342,7 +342,7 @@ class Category
     private $id;
     private $name;
 
-    public function __construct($id, $name)
+    public function construct($id, $name)
     {
         $this->id = $id;
         $this->name = $name;
@@ -421,7 +421,7 @@ class Comment
     private $author;
     private $createDate;
 
-    public function __construct($id, $articleId, $content, $author, $createDate)
+    public function construct($id, $articleId, $content, $author, $createDate)
     {
         $this->id = $id;
         $this->articleId = $articleId;
@@ -528,7 +528,7 @@ class Rating
 
     const ARTICLE_NOT_FOUND = 'Article not found';
 
-    public function __construct($articleId, $rating)
+    public function construct($articleId, $rating)
     {
         $this->articleId = $articleId;
         $this->rating = $rating;
@@ -550,7 +550,7 @@ class Review
     private $articleId;
     private $review;
 
-    public function __construct($articleId, $review)
+    public function construct($articleId, $review)
     {
         $this->articleId = $articleId;
         $this->review = $review;
@@ -573,7 +573,7 @@ class UserFeed
     private $users;
     private $articles;
 
-    public function __construct($users, $articles)
+    public function construct($users, $articles)
     {
         $this->users = $users;
         $this->articles = $articles;
@@ -621,7 +621,7 @@ class UserFeedManager
 {
     private $userFeed; // Instance of the UserFeed
     
-    public function __construct($userId, $article)
+    public function construct($userId, $article)
     {
         $this->userFeed = new UserFeed($userId, $article);
     }
@@ -647,7 +647,544 @@ class UserFeedManager
     {
         echo "Notification sent to user $userId: New article published - {$article['title']}\n";
     }
+
+    public function getCountries()
+    {
+
+        $countries = array(
+            'AF' =>'Afghanistan',
+            'AL' =>'Albania',
+            'DZ' =>'Algeria',
+            'AS' =>'American Samoa',
+            'AD' =>'Andorra',
+            'AO' =>'Angola',
+            'AI' =>'Anguilla',
+            'AQ' =>'Antarctica',
+            'AG' =>'Antigua and Barbuda',
+            'AR' =>'Argentina',
+            'AM' =>'Armenia',
+            'AW' =>'Aruba',
+            'AU' =>'Australia',
+            'AT' =>'Austria',
+            'AZ' =>'Azerbaijan',
+            'BS' =>'Bahamas',
+            'BH' =>'Bahrain',
+            'BD' =>'Bangladesh',
+            'BB' =>'Barbados',
+            'BY' =>'Belarus',
+            'BE' =>'Belgium',
+            'BZ' =>'Belize',
+            'BJ' =>'Benin',
+            'BM' =>'Bermuda',
+            'BT' =>'Bhutan',
+            'BO' =>'Bolivia',
+            'BA' =>'Bosnia and Herzegovina',
+            'BW' =>'Botswana',
+            'BV' =>'Bouvet Island',
+            'BR' =>'Brazil',
+            'BQ' =>'British Antarctic Territory',
+            'IO' =>'British Indian Ocean Territory',
+            'VG' =>'British Virgin Islands',
+            'BN' =>'Brunei',
+            'BG' =>'Bulgaria',
+            'BF' =>'Burkina Faso',
+            'BI' =>'Burundi',
+            'KH' =>'Cambodia',
+            'CM' =>'Cameroon',
+            'CA' =>'Canada',
+            'CT' =>'Canton and Enderbury Islands',
+            'CV' =>'Cape Verde',
+            'KY' =>'Cayman Islands',
+            'CF' =>'Central African Republic',
+            'TD' =>'Chad',
+            'CL' =>'Chile',
+            'CN' =>'China',
+            'CX' =>'Christmas Island',
+            'CC' =>'Cocos [Keeling] Islands',
+            'CO' =>'Colombia',
+            'KM' =>'Comoros',
+            'CG' =>'Congo - Brazzaville',
+            'CD' =>'Congo - Kinshasa',
+            'CK' =>'Cook Islands',
+            'CR' =>'Costa Rica',
+            'HR' =>'Croatia',
+            'CU' =>'Cuba',
+            'CY' =>'Cyprus',
+            'CZ' =>'Czech Republic',
+            'CI' =>'Côte d’Ivoire',
+            'DK' =>'Denmark',
+            'DJ' =>'Djibouti',
+            'DM' =>'Dominica',
+            'DO' =>'Dominican Republic',
+            'NQ' =>'Dronning Maud Land',
+            'DD' =>'East Germany',
+            'EC' =>'Ecuador',
+            'EG' =>'Egypt',
+            'SV' =>'El Salvador',
+            'GQ' =>'Equatorial Guinea',
+            'ER' =>'Eritrea',
+            'EE' =>'Estonia',
+            'ET' =>'Ethiopia',
+            'FK' =>'Falkland Islands',
+            'FO' =>'Faroe Islands',
+            'FJ' =>'Fiji',
+            'FI' =>'Finland',
+            'FR' =>'France',
+            'GF' =>'French Guiana',
+            'PF' =>'French Polynesia',
+            'TF' =>'French Southern Territories',
+            'FQ' =>'French Southern and Antarctic Territories',
+            'GA' =>'Gabon',
+            'GM' =>'Gambia',
+            'GE' =>'Georgia',
+            'DE' =>'Germany',
+            'GH' =>'Ghana',
+            'GI' =>'Gibraltar',
+            'GR' =>'Greece',
+            'GL' =>'Greenland',
+            'GD' =>'Grenada',
+            'GP' =>'Guadeloupe',
+            'GU' =>'Guam',
+            'GT' =>'Guatemala',
+            'GG' =>'Guernsey',
+            'GN' =>'Guinea',
+            'GW' =>'Guinea-Bissau',
+            'GY' =>'Guyana',
+            'HT' =>'Haiti',
+            'HM' =>'Heard Island and McDonald Islands',
+            'HN' =>'Honduras',
+            'HK' =>'Hong Kong SAR China',
+            'HU' =>'Hungary',
+            'IS' =>'Iceland',
+            'IN' =>'India',
+            'ID' =>'Indonesia',
+            'IR' =>'Iran',
+            'IQ' =>'Iraq',
+            'IE' =>'Ireland',
+            'IM' =>'Isle of Man',
+            'IL' =>'Israel',
+            'IT' =>'Italy',
+            'JM' =>'Jamaica',
+            'JP' =>'Japan',
+            'JE' =>'Jersey',
+            'JT' =>'Johnston Island',
+            'JO' =>'Jordan',
+            'KZ' =>'Kazakhstan',
+            'KE' =>'Kenya',
+            'KI' =>'Kiribati',
+            'KW' =>'Kuwait',
+            'KG' =>'Kyrgyzstan',
+            'LA' =>'Laos',
+            'LV' =>'Latvia',
+            'LB' =>'Lebanon',
+            'LS' =>'Lesotho',
+            'LR' =>'Liberia',
+            'LY' =>'Libya',
+            'LI' =>'Liechtenstein',
+            'LT' =>'Lithuania',
+            'LU' =>'Luxembourg',
+            'MO' =>'Macau SAR China',
+            'MK' =>'Macedonia',
+            'MG' =>'Madagascar',
+            'MW' =>'Malawi',
+            'MY' =>'Malaysia',
+            'MV' =>'Maldives',
+            'ML' =>'Mali',
+            'MT' =>'Malta',
+            'MH' =>'Marshall Islands',
+            'MQ' =>'Martinique',
+            'MR' =>'Mauritania',
+            'MU' =>'Mauritius',
+            'YT' =>'Mayotte',
+            'FX' =>'Metropolitan France',
+            'MX' =>'Mexico',
+            'FM' =>'Micronesia',
+            'MI' =>'Midway Islands',
+            'MD' =>'Moldova',
+            'MC' =>'Monaco',
+            'MN' =>'Mongolia',
+            'ME' =>'Montenegro',
+            'MS' =>'Montserrat',
+            'MA' =>'Morocco',
+            'MZ' =>'Mozambique',
+            'MM' =>'Myanmar [Burma]',
+            'NA' =>'Namibia',
+            'NR' =>'Nauru',
+            'NP' =>'Nepal',
+            'NL' =>'Netherlands',
+            'AN' =>'Netherlands Antilles',
+            'NT' =>'Neutral Zone',
+            'NC' =>'New Caledonia',
+            'NZ' =>'New Zealand',
+            'NI' =>'Nicaragua',
+            'NE' =>'Niger',
+            'NG' =>'Nigeria',
+            'NU' =>'Niue',
+            'NF' =>'Norfolk Island',
+            'KP' =>'North Korea',
+            'VD' =>'North Vietnam',
+            'MP' =>'Northern Mariana Islands',
+            'NO' =>'Norway',
+            'OM' =>'Oman',
+            'PC' =>'Pacific Islands Trust Territory',
+            'PK' =>'Pakistan',
+            'PW' =>'Palau',
+            'PS' =>'Palestinian Territories',
+            'PA' =>'Panama',
+            'PZ' =>'Panama Canal Zone',
+            'PG' =>'Papua New Guinea',
+            'PY' =>'Paraguay',
+            'YD' =>'People\'s Democratic Republic of Yemen',
+            'PE' =>'Peru',
+            'PH' =>'Philippines',
+            'PN' =>'Pitcairn Islands',
+            'PL' =>'Poland',
+            'PT' =>'Portugal',
+            'PR' =>'Puerto Rico',
+            'QA' =>'Qatar',
+            'RO' =>'Romania',
+            'RU' =>'Russia',
+            'RW' =>'Rwanda',
+            'BL' =>'Saint Barthélemy',
+            'SH' =>'Saint Helena',
+            'KN' =>'Saint Kitts and Nevis',
+            'LC' =>'Saint Lucia',
+            'MF' =>'Saint Martin',
+            'PM' =>'Saint Pierre and Miquelon',
+            'VC' =>'Saint Vincent and the Grenadines',
+            'WS' =>'Samoa',
+            'SM' =>'San Marino',
+            'SA' =>'Saudi Arabia',
+            'SN' =>'Senegal',
+            'RS' =>'Serbia',
+            'CS' =>'Serbia and Montenegro',
+            'SC' =>'Seychelles',
+            'SL' =>'Sierra Leone',
+            'SG' =>'Singapore',
+            'SK' =>'Slovakia',
+            'SI' =>'Slovenia',
+            'SB' =>'Solomon Islands',
+            'SO' =>'Somalia',
+            'ZA' =>'South Africa',
+            'GS' =>'South Georgia and the South Sandwich Islands',
+            'KR' =>'South Korea',
+            'ES' =>'Spain',
+            'LK' =>'Sri Lanka',
+            'SD' =>'Sudan',
+            'SR' =>'Suriname',
+            'SJ' =>'Svalbard and Jan Mayen',
+            'SZ' =>'Swaziland',
+            'SE' =>'Sweden',
+            'CH' =>'Switzerland',
+            'SY' =>'Syria',
+            'ST' =>'São Tomé and Príncipe',
+            'TW' =>'Taiwan',
+            'TJ' =>'Tajikistan',
+            'TZ' =>'Tanzania',
+            'TH' =>'Thailand',
+            'TL' =>'Timor-Leste',
+            'TG' =>'Togo',
+            'TK' =>'Tokelau',
+            'TO' =>'Tonga',
+            'TT' =>'Trinidad and Tobago',
+            'TN' =>'Tunisia',
+            'TR' =>'Turkey',
+            'TM' =>'Turkmenistan',
+            'TC' =>'Turks and Caicos Islands',
+            'TV' =>'Tuvalu',
+            'UM' =>'U.S. Minor Outlying Islands',
+            'PU' =>'U.S. Miscellaneous Pacific Islands',
+            'VI' =>'U.S. Virgin Islands',
+            'UG' =>'Uganda',
+            'UA' =>'Ukraine',
+            'SU' =>'Union of Soviet Socialist Republics',
+            'AE' =>'United Arab Emirates',
+            'GB' =>'United Kingdom',
+            'US' =>'United States',
+            'ZZ' =>'Unknown or Invalid Region',
+            'UY' =>'Uruguay',
+            'UZ' =>'Uzbekistan',
+            'VU' =>'Vanuatu',
+            'VA' =>'Vatican City',
+            'VE' =>'Venezuela',
+            'VN' =>'Vietnam',
+            'WK' =>'Wake Island',
+            'WF' =>'Wallis and Futuna',
+            'EH' =>'Western Sahara',
+            'YE' =>'Yemen',
+            'ZM' =>'Zambia',
+            'ZW' =>'Zimbabwe',
+            'AX' =>'Åland Islands',
+        );
+        
+        /**
+         * Filter the countries before returning
+         *
+         * @param array $countries countries array for filtering
+         */
+        $countries = countryFilters('example_countries_filters', $countries);
+    
+        /**
+         * Return the translated and filtered countries
+         */
+        return $countries;
+    }
+
+    public function countryFilters($countries)
+    {
+    		// add a translatable country:
+    		$countries['XX'] = ('Country Name');
+    		// remove a country:
+    		unset($countries['YY']);
+    		// modify a country:
+    		$countries['US'] = ('United States of America');
+    		// always return countries!
+    		return $countries;
+    }
 }
+
+//Dummy Data
+$countriesDummy=array(
+    "Afghanistan",
+    "Albania",
+    "Algeria",
+    "American Samoa",
+    "Andorra",
+    "Angola",
+    "Anguilla",
+    "Antarctica",
+    "Antigua and Barbuda",
+    "Argentina",
+    "Armenia",
+    "Aruba",
+    "Australia",
+    "Austria",
+    "Azerbaijan",
+    "Bahamas",
+    "Bahrain",
+    "Bangladesh",
+    "Barbados",
+    "Belarus",
+    "Belgium",
+    "Belize",
+    "Benin",
+    "Bermuda",
+    "Bhutan",
+    "Bolivia",
+    "Bosnia and Herzegowina",
+    "Botswana",
+    "Bouvet Island",
+    "Brazil",
+    "British Indian Ocean Territory",
+    "Brunei Darussalam",
+    "Bulgaria",
+    "Burkina Faso",
+    "Burundi",
+    "Cambodia",
+    "Cameroon",
+    "Canada",
+    "Cape Verde",
+    "Cayman Islands",
+    "Central African Republic",
+    "Chad",
+    "Chile",
+    "China",
+    "Christmas Island",
+    "Cocos (Keeling) Islands",
+    "Colombia",
+    "Comoros",
+    "Congo",
+    "Congo, the Democratic Republic of the",
+    "Cook Islands",
+    "Costa Rica",
+    "Cote d'Ivoire",
+    "Croatia (Hrvatska)",
+    "Cuba",
+    "Cyprus",
+    "Czech Republic",
+    "Denmark",
+    "Djibouti",
+    "Dominica",
+    "Dominican Republic",
+    "East Timor",
+    "Ecuador",
+    "Egypt",
+    "El Salvador",
+    "Equatorial Guinea",
+    "Eritrea",
+    "Estonia",
+    "Ethiopia",
+    "Falkland Islands (Malvinas)",
+    "Faroe Islands",
+    "Fiji",
+    "Finland",
+    "France",
+    "France Metropolitan",
+    "French Guiana",
+    "French Polynesia",
+    "French Southern Territories",
+    "Gabon",
+    "Gambia",
+    "Georgia",
+    "Germany",
+    "Ghana",
+    "Gibraltar",
+    "Greece",
+    "Greenland",
+    "Grenada",
+    "Guadeloupe",
+    "Guam",
+    "Guatemala",
+    "Guinea",
+    "Guinea-Bissau",
+    "Guyana",
+    "Haiti",
+    "Heard and Mc Donald Islands",
+    "Holy See (Vatican City State)",
+    "Honduras",
+    "Hong Kong",
+    "Hungary",
+    "Iceland",
+    "India",
+    "Indonesia",
+    "Iran (Islamic Republic of)",
+    "Iraq",
+    "Ireland",
+    "Israel",
+    "Italy",
+    "Jamaica",
+    "Japan",
+    "Jordan",
+    "Kazakhstan",
+    "Kenya",
+    "Kiribati",
+    "Korea, Democratic People's Republic of",
+    "Korea, Republic of",
+    "Kuwait",
+    "Kyrgyzstan",
+    "Lao, People's Democratic Republic",
+    "Latvia",
+    "Lebanon",
+    "Lesotho",
+    "Liberia",
+    "Libyan Arab Jamahiriya",
+    "Liechtenstein",
+    "Lithuania",
+    "Luxembourg",
+    "Macau",
+    "Macedonia, The Former Yugoslav Republic of",
+    "Madagascar",
+    "Malawi",
+    "Malaysia",
+    "Maldives",
+    "Mali",
+    "Malta",
+    "Marshall Islands",
+    "Martinique",
+    "Mauritania",
+    "Mauritius",
+    "Mayotte",
+    "Mexico",
+    "Micronesia, Federated States of",
+    "Moldova, Republic of",
+    "Monaco",
+    "Mongolia",
+    "Montserrat",
+    "Morocco",
+    "Mozambique",
+    "Myanmar",
+    "Namibia",
+    "Nauru",
+    "Nepal",
+    "Netherlands",
+    "Netherlands Antilles",
+    "New Caledonia",
+    "New Zealand",
+    "Nicaragua",
+    "Niger",
+    "Nigeria",
+    "Niue",
+    "Norfolk Island",
+    "Northern Mariana Islands",
+    "Norway",
+    "Oman",
+    "Pakistan",
+    "Palau",
+    "Panama",
+    "Papua New Guinea",
+    "Paraguay",
+    "Peru",
+    "Philippines",
+    "Pitcairn",
+    "Poland",
+    "Portugal",
+    "Puerto Rico",
+    "Qatar",
+    "Reunion",
+    "Romania",
+    "Russian Federation",
+    "Rwanda",
+    "Saint Kitts and Nevis",
+    "Saint Lucia",
+    "Saint Vincent and the Grenadines",
+    "Samoa",
+    "San Marino",
+    "Sao Tome and Principe",
+    "Saudi Arabia",
+    "Senegal",
+    "Seychelles",
+    "Sierra Leone",
+    "Singapore",
+    "Slovakia (Slovak Republic)",
+    "Slovenia",
+    "Solomon Islands",
+    "Somalia",
+    "South Africa",
+    "South Georgia and the South Sandwich Islands",
+    "Spain",
+    "Sri Lanka",
+    "St. Helena",
+    "St. Pierre and Miquelon",
+    "Sudan",
+    "Suriname",
+    "Svalbard and Jan Mayen Islands",
+    "Swaziland",
+    "Sweden",
+    "Switzerland",
+    "Syrian Arab Republic",
+    "Taiwan, Province of China",
+    "Tajikistan",
+    "Tanzania, United Republic of",
+    "Thailand",
+    "Togo",
+    "Tokelau",
+    "Tonga",
+    "Trinidad and Tobago",
+    "Tunisia",
+    "Turkey",
+    "Turkmenistan",
+    "Turks and Caicos Islands",
+    "Tuvalu",
+    "Uganda",
+    "Ukraine",
+    "United Arab Emirates",
+    "United Kingdom",
+    "United States",
+    "United States Minor Outlying Islands",
+    "Uruguay",
+    "Uzbekistan",
+    "Vanuatu",
+    "Venezuela",
+    "Vietnam",
+    "Virgin Islands (British)",
+    "Virgin Islands (U.S.)",
+    "Wallis and Futuna Islands",
+    "Western Sahara",
+    "Yemen",
+    "Yugoslavia",
+    "Zambia",
+    "Zimbabwe"
+);
 
 // Usage Example
 $userDatabase = new UserDatabase();
@@ -913,7 +1450,7 @@ $articleToNotify = [
     'title' => 'Breaking News',
     'category' => 'Politics',
     'author' => 'John Smith',
-    'country' => 'USA'
+    'country' => $countriesDummy['13']
 ];
 
 $userFeed->notifyUsers($articleToNotify);
